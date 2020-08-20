e87372d2c29c382943b0ad500d74db9a0bb6aab7
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2020-08-20 13:42:47 +0000

    allow increase oftile_scale for bigger countries

--- scripts/zonal_computation.py
+++ scripts/zonal_computation.py
@@ -6,6 +6,10 @@ import csv
 import pandas as pd
 import bqplot as bq
 import json
+import io
+from contextlib import redirect_stdout
+from sepal_ui.scripts import utils as su
+import time
 
 ee.Initialize()
 
@@ -25,6 +29,8 @@ def run_zonal_computation(country_code, Map, output):
     ###################################
     ###      placer sur la map     ####
     ###################################
+    su.displayIO(output, 'viszualiser data')
+    
     country = ee.FeatureCollection('USDOS/LSIB_SIMPLE/2017').filter(ee.Filter.eq('country_co', country_code))
     Map.addLayer(country, {}, 'country_code')
     Map.centerObject(country)
@@ -90,77 +96,122 @@ def run_zonal_computation(country_code, Map, output):
 
     #export raw data
     out_dir = os.path.join(os.path.expanduser('~'), 'downloads')
-    raw_stats = os.path.join(out_dir, country_code+'_raw.csv') 
-    geemap.zonal_statistics(country_gfcc_2010, country_gez_2010_vector, raw_stats, statistics_type='FIXED_HIST', scale=100, crs=getConformProj(), hist_min=0, hist_max=100, hist_steps=100)
-
+    raw_stats = os.path.join(resultDir, country_code + '_raw.csv') 
+    
+    if not os.path.isfile(raw_stats):
+        print('compute the zonal analysis')
+        #compute the zonal analysis
+        computation = False
+        cpt = 0
+        while not computation:
+            f = io.StringIO()
+            with redirect_stdout(f):
+                geemap.zonal_statistics(
+                    in_value_raster = country_gfcc_2010, 
+                    in_zone_vector  = country_gez_2010_vector, 
+                    out_file_path   = raw_stats, 
+                    statistics_type = 'FIXED_HIST', 
+                    scale           = 100, 
+                    crs             = getConformProj(), 
+                    hist_min        = 0, 
+                    hist_max        = 100, 
+                    hist_steps      = 100,
+                    tile_scale      = 2**cpt
+                )
+            su.displayIO(output, f.getvalue())
+        
+            #check if the computation have finished on a file 
+            if not os.path.isfile(raw_stats):
+                #cannot use tile_scale > 16 
+                if cpt == 3:
+                    raise Exception("Aoi is to big")
+                
+                #change the tile_scale and relaunch the process
+                su.displayIO(output, f.getvalue(), 'error')
+                time.sleep(2)
+                cpt += 1
+                su.displayIO(output, 'on augmente le tile_scale ({})'.format(2**cpt), 'warning')
+                time.sleep(2)
+            else:
+                computation = True
 
     #######################################
     ###  lire et concatener les données ###
     #######################################
-    data = pd.read_csv(raw_stats, sep=',')
-
-    #enlever les lignes Nan 
-    #(vérifier dans le tableau d'avant qu'elles ne sont pas trop grandes)
-    data = data.dropna()
-
-    #recuperer les valeurs de label
-    ecozones = data.label.unique()
-
-    #aggreger les lignes avec les même valeurs 
-    dummy = []
-    for i in range(100):
-        dummy.append("{:.1f}".format(i))
-    stats = pd.DataFrame(dummy, columns=['treecover'])
-
-    for index, ecozone in enumerate(ecozones): 
-        patches = data.loc[data.label == ecozone]
-        label = []
-        for i in range(100):
-            label.append(["{:.1f}".format(i), 0])
-        
-        for index, row in patches.iterrows():
-            tmp = json.loads(row['histogram'])        
-            for index, value in enumerate(tmp):
-                label[index][1] += value[1]
-            
-        label = pd.DataFrame(label, columns=['treecover', list_zones[ecozone]])
-        stats = pd.merge(left=stats, right=label, left_on='treecover', right_on='treecover')
     
-    #############################
-    ####   exporter     #########
-    #############################
     out_stats = os.path.join(resultDir, country_code + '_stats.csv') 
-    stats.to_csv(out_stats, index=False) 
+    
+    if not os.path.isfile(out_stats):
+        su.displayIO(output, 'lire et concatener les données')
+        data = pd.read_csv(raw_stats, sep=',')
+
+        su.displayIO(output, 'retirer les no_data')
+        #enlever les lignes Nan 
+        #(vérifier dans le tableau d'avant qu'elles ne sont pas trop grandes)
+        data = data.dropna()
+        
+        #recuperer les valeurs de label
+        ecozones = data.label.unique()
+
+        su.displayIO(output, 'merge')
+        #aggreger les lignes avec les même valeurs 
+        dummy = []
+        for i in range(100):
+            dummy.append("{:.1f}".format(i))
+        stats = pd.DataFrame(dummy, columns=['treecover'])
+
+        su.displayIO(output, 'labeliser')
+        for index, ecozone in enumerate(ecozones): 
+            patches = data.loc[data.label == ecozone]
+            label = []
+            for i in range(100):
+                label.append(["{:.1f}".format(i), 0])
+        
+            for index, row in patches.iterrows():
+                tmp = json.loads(row['histogram'])        
+                for index, value in enumerate(tmp):
+                    label[index][1] += value[1]
+            
+            label = pd.DataFrame(label, columns=['treecover', list_zones[ecozone]])
+            stats = pd.merge(left=stats, right=label, left_on='treecover', right_on='treecover')
+            
+        #exporter 
+        stats.to_csv(out_stats, index=False) 
     
     #############################
     ##    tracer les figures ####
     #############################
+    su.displayIO(output, 'tracer les figures')
+    
+    stats = pd.read_csv(out_stats)
+    
+    #recuperer les noms de label
+    ecozones = stats.columns[1:]
 
     x_sc = bq.OrdinalScale()
     ax_x = bq.Axis(label='treecover', scale=x_sc)
-
+    
     x= []
     for i in range(100):
         x.append(i)
-
+    
     figs = []
     for ecozone in ecozones:
-        y_sc = bq.LinearScale(max=stats[list_zones[ecozone]].max())
+        y_sc = bq.LinearScale(max=stats[ecozone].max())
         ax_y = bq.Axis(label='surface (px)', scale=y_sc, orientation='vertical')
         y = []
         for index, row in stats.iterrows():
-            y.append(row[list_zones[ecozone]])
+            y.append(row[ecozone])
     
         mark = bq.Bars(x=x, y=y, scales={'x': x_sc, 'y': y_sc})
     
         fig_hist = bq.Figure(
-            title=list_zones[ecozone],
+            title=ecozone,
             marks=[mark], 
             axes=[ax_x, ax_y]
         )
     
         figs.append(fig_hist)
-
     
     return figs, out_stats
 
